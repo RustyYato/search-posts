2d383f1add0240addf4aea01d677794cb4ff4087
RustyYato <krishna.sd.2012@gmail.com>
2020-11-08 12:37:07 -0500

    remove hasher from inner maps

--- src/map.rs
+++ src/map.rs
@@ -1,12 +1,15 @@
 use ahash::RandomState;
-use hashbrown::HashMap;
+use hashbrown::{hash_map::RawEntryMut, HashMap};
 use std::iter::Flatten;
 use std::vec::IntoIter;
 
 use std::hash::{BuildHasher, Hash, Hasher};
 
+#[derive(Clone, Copy)]
+pub struct NoHash;
+
 pub struct Map {
-    inner: Box<[HashMap<crate::PhraseBuf, u32>]>,
+    inner: Box<[HashMap<crate::PhraseBuf, u32, NoHash>]>,
     hasher: RandomState,
 }
 
@@ -14,12 +17,12 @@ impl Map {
     pub fn new() -> Self {
         Self {
             hasher: RandomState::new(),
-            inner: vec![HashMap::new(); 64].into_boxed_slice(),
+            inner: vec![HashMap::with_hasher(NoHash); 64].into_boxed_slice(),
         }
     }
 
-    fn hash<H: Hash>(&self, value: &H) -> (u64, usize) {
-        let mut hasher = self.hasher.build_hasher();
+    fn hash<H: Hash>(hasher: &RandomState, value: &H) -> (u64, usize) {
+        let mut hasher = hasher.build_hasher();
         value.hash(&mut hasher);
         let hash = hasher.finish();
         let index = (hash & (64 - 1)) as usize;
@@ -27,32 +30,41 @@ impl Map {
     }
 
     pub fn add(&mut self, phrase: crate::Phrase, value: u32) {
-        let (hash, index) = self.hash(&phrase);
+        let hasher = &self.hasher;
+        let (hash, index) = Self::hash(hasher, &phrase);
 
-        *self.inner[index]
-            .raw_entry_mut()
-            .from_hash(hash, |key| {
-                key.iter()
-                    .map(AsRef::<str>::as_ref)
-                    .eq(phrase.iter().copied())
-            })
-            .or_insert_with(|| (crate::config::to_owned(phrase), 0))
-            .1 += value;
+        match self.inner[index].raw_entry_mut().from_hash(hash, |key| {
+            key.iter()
+                .map(AsRef::<str>::as_ref)
+                .eq(phrase.iter().copied())
+        }) {
+            RawEntryMut::Occupied(entry) => *entry.into_mut() += value,
+            RawEntryMut::Vacant(entry) => {
+                entry.insert_with_hasher(hash, crate::config::to_owned(phrase), value, |x| {
+                    Self::hash(hasher, x).0
+                });
+            }
+        }
     }
 
     pub fn add_owned(&mut self, phrase: crate::PhraseBuf, value: u32) {
-        let (hash, index) = self.hash(&phrase);
+        let hasher = &self.hasher;
+        let (hash, index) = Self::hash(hasher, &phrase);
 
-        *self.inner[index]
+        match self.inner[index]
             .raw_entry_mut()
             .from_hash(hash, |key| *key == phrase)
-            .or_insert_with(|| (phrase, 0))
-            .1 += value;
+        {
+            RawEntryMut::Occupied(entry) => *entry.into_mut() += value,
+            RawEntryMut::Vacant(entry) => {
+                entry.insert_with_hasher(hash, phrase, value, |x| Self::hash(hasher, x).0);
+            }
+        }
     }
 }
 
 impl IntoIterator for Map {
-    type IntoIter = Flatten<IntoIter<HashMap<crate::PhraseBuf, u32>>>;
+    type IntoIter = Flatten<IntoIter<HashMap<crate::PhraseBuf, u32, NoHash>>>;
     type Item = (crate::PhraseBuf, u32);
 
     fn into_iter(self) -> Self::IntoIter {
